7210fcd5245cf4651b1f6dae930b13c3d6a00b72
jagathd <80244815+jagathd@users.noreply.github.com>
2024-02-02 10:37:45 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -6,14 +6,6 @@ from pyamaze import maze,agent
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
     
     m=maze(20,20)
     m.CreateMaze(loopPercent=50)
